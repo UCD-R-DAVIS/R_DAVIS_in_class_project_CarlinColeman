e42fa6e08b4ecd93d3097f150457bfc719e4235b
Carlin Coleman <carcoleman@ucdavis.edu>
2024-10-03 13:58:28 -0700

    Elefantes

---
+++ Week2_IntrotoR.R
@@ -0,0 +1,56 @@
+4 + 8 * 3 ^ 2
+#writing a comment
+3+2
+3+2
+#scientific notation
+2/100000
+
+#mathematical functions
+exp(1)
+
+?log
+?log
+log(2,4)
+log(4,2)
+log(base=4, x=2)
+
+x<-1
+x
+rm(x)
+
+#nested function
+sqrt(e^4)
+sqrt(exp(4))
+log(exp(100))
+
+#six comparison functions
+mynumber <- 6
+mynumber == 5
+
+
+#objects and assignments
+mynumber <- 7
+othernumber <- -3
+mynumber*othernumber
+
+#object name conventions
+numSamples <- 50
+num_samples <- 40
+rm(num_samples)
+
+#tab completion
+
+#errors and warnings
+log("a_word")
+
+log_of_negative <- log(-2)
+
+#challenge
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+
+elephant1_lb <- elephant1_kg*2.2
+elephant2_lb > elephant1_lb
+
+myelephants <- c(elephant1_lb, elephant2_lb)
+which(myelephants==max(myelephants))
